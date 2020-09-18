6b9096fa4808f5bbe406f5cd3935b8a9f5a1a085
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-09-18 19:41:12 +0300

    Working main mechanic.

--- Homework_19.09./arrays/Exercise_6.php
+++ Homework_19.09./arrays/Exercise_6.php
@@ -30,28 +30,27 @@ foreach ($hiddenSplittedRandomWordArray as &$letter) {
 while ($win == false) {
     echo "\n";
     echo implode(" ", $hiddenSplittedRandomWordArray) . PHP_EOL;
-    print_r($hiddenSplittedRandomWordArray);
     $input = readline("\nChoose letter: ");
-    $key = array_search($input, $splittedRandomWordArray); // Returns index (key).
 
     // If input is present in the array...
-    if (in_array($input, $splittedRandomWordArray)) {
-        
-        // Search the index of the input in this array
-        
-        echo $key;
-        $hiddenSplittedRandomWordArray[$key] = $input;
+    if (in_array($input, $splittedRandomWordArray)) { //returns bool {
+
+        // Search the indexes of the input in this array
+        $keys = array_keys($splittedRandomWordArray, $input); // Returns indexes (keys).
+
+            foreach ($keys as &$letter) {
+                $hiddenSplittedRandomWordArray[$letter] = $input;
+            }
+
         echo "CORRECT!\n";
-        
-        
-    } elseif (in_array($input, $splittedRandomWordArray) == false) {
+    } elseif (!in_array($input, $splittedRandomWordArray)) {
 
         echo "\nWrong letter!\n";
-
     }
 
     // Determines win
     if ($splittedRandomWordArray == $hiddenSplittedRandomWordArray) {
+        echo implode(" ", $hiddenSplittedRandomWordArray) . PHP_EOL;
         echo "\nYOU WIN!\n";
         echo "\nThe correct word is: " . implode(" ", $hiddenSplittedRandomWordArray) . PHP_EOL;
 
