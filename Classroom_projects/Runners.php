5dc91cadc642fc16ceaa88aacfb5370ce29ffc1a
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-09-20 21:35:50 +0300

    Runners.php updated with working main logic.

--- Classroom_projects/Runners.php
+++ Classroom_projects/Runners.php
@@ -6,77 +6,45 @@
 // When one player moves 1km, his speed and distance is outputted
 // When one player gets to finish, he is declared as a winner
 
-abstract class Runners
-{
-    public abstract function getName(): string;
-    public abstract function getSpeed(): float;
-}
+$field1 = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
+$field2 = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
 
-class Runner extends Runners
-{
-    private string $name;
-    private float $speed; // m/s
 
-    public function __construct($name, $speed)
-    {
-        $this->name = $name;
-        $this->speed = $speed;
+
+$player1 = "X";
+$player2 = "O";
+$position1 = 0;
+$position2 = 0;
+$speed1 = rand(1, 4);
+$speed2 = rand(1, 4);
+$finishLine = false;
+
+while ($finishLine == false) {
+
+    $field1[$position1] = $player1;
+    $field2[$position2] = $player2;
+
+    echo "Track 1 | " . implode(" ", $field1) . PHP_EOL;
+    echo "Track 2 | " . implode(" ", $field2) . PHP_EOL;
+    echo "\n";
+
+    $position1 += $speed1;
+    $position2 += $speed2;
+    
+    $field1[$position1 - $speed1] = "_";
+    $field2[$position2 - $speed2] = "_";
+
+    if ($position1 > 21 && $position2 < 21) {
+        echo "The winner is $player1!\n";
+        $finishLine = true;
+    } elseif ($position1 < 21 && $position2 > 21) {
+        echo "The winner is $player2!\n";
+        $finishLine = true;
+    } elseif ($position1 > 21 && $position2 > 21) {
+        echo "It's a tie!\n";
+        $finishLine = true;
     }
+    
+    sleep(1);
 
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getSpeed(): float
-    {
-        return $this->speed;
-    }
 }
-
-$runner1 = new Runner("X", 10.0);
-$runner2 = new Runner("0", 9.2);
-
-$field1 = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
-$field2 = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
-
-$hundredMeters = "_";
-
-// Set starting possition
-if ($field1 == true && $field2 == true) {
-    array_unshift($field1, $runner1->getName());
-    array_unshift($field2, $runner2->getName());
-}
-
-$playerPosition = 0;
-
-while(
-
-)
-
-
-
-// Defines movement
-// for ($i = 0; $i <= count($field1); $i++) {
-//     $field1[$i] = $runner1->getName();
-//     $field1[$i - 1] = "_";
-
-//     echo implode(" ", $field1) . PHP_EOL;
-
-//     if ($i > count($field1)) {
-//         break;
-//     }
-//     sleep(1);
-
-//     for ($j = 0; $j <= count($field2); $j++) {
-//         $field2[$j] = $runner2->getName();
-//         $field2[$i - 1] = "_";
-
-//         echo implode(" ", $field2) . PHP_EOL;
-//         if ($j > count($field2)) {
-//             break;
-//         }
-//         sleep(1);
-//     break;
-//     }
-// }
