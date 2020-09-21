5516f1638ac6a33a968f0ab7cb6f18d7ebdba9e1
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-09-21 20:50:53 +0300

    Reverted changes.

--- Classroom_projects/13_Files/index.php
+++ Classroom_projects/13_Files/index.php
@@ -21,10 +21,10 @@ same naming classes with different namespace.
 // Index.php
 
 require_once 'src/Spices/Spice.php';
+require_once 'src/Spices/Paprika/Paprika.php';
+require_once 'src/Spices/Salt/Salt.php';
 require_once 'src/Spices/SpicesCollection.php';
-require_once 'src/autoload.php';
-includeClass($classes);
-includeClass($classes);
+
 
 use src\Spices\Paprika;
 use src\Spices\Salt;
