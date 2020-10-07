8e106433658f6493ef232d347f8a6ce32acb1b21
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-10-07 15:55:16 +0300

    Update

--- Classroom_projects/19_GET_POST_Colors/index.php
+++ Classroom_projects/19_GET_POST_Colors/index.php
@@ -1,25 +1,31 @@
 <?php
 
-function randColor() {
-    return '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
+function randColor()
+{
+    return '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT); // Random color generator
 }
 
-$color = $_GET[randColor()] ?? 'red';
-$defaultSquare = 50;
-$addingDimension = $defaultSquare / 4;
-$newDimension = $defaultSquare + $addingDimension;
+$color = $_GET[randColor()] ?? randColor();
+$defaultSize = $_GET['size'] ? $_GET['size'] + ($_GET['size'] / 4) : 50;
+// If $_GET['size'] is 'true', 'size' + 'size' / 4, or the it will be 50;
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<div style="width: <?= $defaultSquare ?>px; height: <?= $defaultSquare ?>px; background-color: <?php echo randColor(); ?>"></div>
+    <form action="/" method="get">
 
-<form action="/" method="get">
-<button type="submit" value=<?php $color ?> name="color">Size up and change color!</button>
-</form>
+        <div style="width: <?= $defaultSize ?>px; height: <?= $defaultSize ?>px; background-color: <?= randColor(); ?>"></div>
+
+        <input type="hidden" name="size" value=<?= $defaultSize; ?>>
+
+        <button type="submit" value=<?= $color ?> name="color">Size up and change color!</button>
+
+    </form>
 
 </body>
+
 </html>
