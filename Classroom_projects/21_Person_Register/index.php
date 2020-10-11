52066dea52284981eb4e5a8000c0b4e967272558
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-10-12 01:36:48 +0300

    Finished project

--- Classroom_projects/21_Person_Register/index.php
+++ Classroom_projects/21_Person_Register/index.php
@@ -3,17 +3,21 @@
 declare(strict_types=1);
 
 require 'PersonProfile.php';
+require_once 'PersonDataStorage.php';
 
 // Sets person data from html post
-$person = new PersonProfile();
-$person->setName($_POST['name'] ?? '');
-$person->setSurname($_POST['surname'] ?? '');
-$person->setPersonCode($_POST['personcode'] ?? '');
-$person->setAddress($_POST['address'] ?? '');
+$person = new PersonProfile(
+    $_POST['name'] ?? 'name',
+    $_POST['surname'] ?? 'surname',
+    $_POST['personcode'] ?? 'personcode',
+    $_POST['address'] ?? 'address'
+);
 
-// Saves person data to CSV file
-$person->saveData();
+$dataStorage = new PersonDataStorage();
+$dataStorage->saveData($person);
 
+$persCode = $_POST['serchedcode'] ?? 'serchedcode';
+$foundedPerson = $dataStorage->searchByPersCode($persCode);
 
 ?>
 
@@ -24,6 +28,7 @@ $person->saveData();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Person register</title>
 
+    <h2>Save the person in the database</h2>
     <form action="/" method="post">
 
         <label for="name">Name:</label>
@@ -32,15 +37,39 @@ $person->saveData();
         <label for="surname">Surname:</label>
         <input type="text" id="surname" name="surname"><br>
 
-        <label for="personcode">Person code:</label>
+        <label for="personcode">Personal code:</label>
         <input type="text" id="personcode" name="personcode"><br>
 
         <label for="address">Address:</label>
         <input type="text" id="address" name="address"><br>
 
-        <button type="submit">Submit</button><br>
+        <button type="submit" name="submit1">Save</button><br>
+        <?php if (isset($_POST["submit1"])) : ?>
+            <p style="font-weight: bold;">Saved!</p>
+        <?php endif; ?>
 
     </form>
+
+    Name: <?= $person->getName(); ?><br>
+    Surname: <?= $person->getSurname(); ?><br>
+    Personal code: <?= $person->getPersonCode(); ?><br>
+    Address: <?= $person->getAddress(); ?><br><br>
+
+    <h2>Look for the person in the database</h2>
+    <form action="/" method="post">
+        <label for="searchpersoncode">Search by personal code:</label>
+        <input type="text" id="searchpersoncode" name="serchedcode"><br>
+        <button type="submit">Search</button><br>
+    </form>
+
+    <?php if ($foundedPerson !== null) : ?>
+        Name: <?= $foundedPerson->getName(); ?><br>
+        Surname: <?= $foundedPerson->getSurname(); ?><br>
+        Personal code: <?= $foundedPerson->getPersonCode(); ?><br>
+        Address: <?= $foundedPerson->getAddress(); ?><br>
+    <?php else : ?>
+        <?= 'Empty' ?>
+    <?php endif; ?>
 
 </head>
 
