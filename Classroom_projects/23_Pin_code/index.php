00109559c0dc9b27ac2eb4f4093c43b94ef996e4
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-10-12 11:31:43 +0300

    Finished pre-class implemented project

---
+++ Classroom_projects/23_Pin_code/index.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+$correctPin = "3399";
+
+if (isset($_POST['button']) && strlen($_SESSION["code"]) < 4) {
+    if ($_SESSION["code"] !== "") {
+        $_SESSION["code"] .= "";
+    }
+
+    $_SESSION["code"] .= $_POST['button'];
+
+} else {
+    $_SESSION["code"] = "";
+}
+
+$splittedPin = str_split($_SESSION["code"]);
+
+if ($_SESSION["code"] === $correctPin) {
+    echo "UNLOCKED";
+} else {
+    echo "LOCKED";
+}
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enter pincode</title>
+</head>
+
+<body>
+    <br>
+    <?php foreach ($splittedPin as $digit) : ?>
+        <?php if ($_SESSION["code"] != null) : ?>
+            <?= "X" ?>
+        <?php endif; ?>
+    <?php endforeach; ?>
+
+    <form action="/" method="post">
+
+        <input type="submit" id="button" name="button" value="1">
+        <input type="submit" id="button" name="button" value="2">
+        <input type="submit" id="button" name="button" value="3"><br>
+        <input type="submit" id="button" name="button" value="4">
+        <input type="submit" id="button" name="button" value="5">
+        <input type="submit" id="button" name="button" value="6"><br>
+        <input type="submit" id="button" name="button" value="7">
+        <input type="submit" id="button" name="button" value="8">
+        <input type="submit" id="button" name="button" value="9"><br>
+        <input type="submit" id="button" name="button" value="0">
+
+
+    </form>
+</body>
+
+</html>
